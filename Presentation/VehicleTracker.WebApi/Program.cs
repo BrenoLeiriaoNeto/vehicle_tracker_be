95457bcb592237f776cb286a6c00f2bf7e189d8f
BrenoLeiriaoNeto <bleiriaoneto@icloud.com>
2026-07-13 16:15:54 -0300

    feat(api): add routing, authentication, and authorization middleware to pipeline

--- Presentation/VehicleTracker.WebApi/Program.cs
+++ Presentation/VehicleTracker.WebApi/Program.cs
@@ -38,6 +38,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 
 await app.ApplyMongoIndexesAsync();
